f4f5439789b855688077f19d2357d7171dbd2e16
wwwbrunagon <wwwbrunagon@gmail.com>
2019-09-23 21:01:20 -0300

    git global config test

--- level_01/biblioteca.py
+++ level_01/biblioteca.py
@@ -1,6 +1,9 @@
-def gera_nome_convite(convite):
+def gera_nome(convite):
   posicao_final = len(convite);
   posicao_inicial = posicao_final - 4
   parte1 = convite[0:4]
   parte2 = convite[posicao_final-4:posicao_final]
-  return parte1 + ' ' + parte2
+  return parte1 + ' ' + parte2 
+
+
+
